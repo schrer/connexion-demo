a616fa39473970e03a680b66ffc36de3b4a937cc
Martin Schröder <ma.schroeder@outlook.com>
2023-09-29 15:20:03 +0200

    Remove Pet class

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import json
 import connexion
 from connexion.resolver import RelativeResolver
-from typing import List
 
-class Pet:
-    def __init__(self, id: int, name: str, tag: str) -> None:
-        self.id = id
-        self.name = name
-        self.tag = tag
-
-def listPets(limit: int) -> List[Pet]:
+def listPets(limit: int):
     p1 = json.loads('{"id":1,"name":"Pet 1","tag":"#pet1"}')
     return [p1]
 
