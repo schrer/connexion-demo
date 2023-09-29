995327abc2cbb1e644f7a248a530d0be95d3e8fc
Martin Schröder <ma.schroeder@outlook.com>
2023-09-29 16:35:48 +0200

    Generate pets, rename operation

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import json
-import connexion
+from connexion import FlaskApp
 from connexion.resolver import RelativeResolver
 
-def listPets(limit: int):
-    p1 = json.loads('{"id":1,"name":"Pet 1","tag":"#pet1"}')
-    return [p1]
+def list_pets(limit: int):
+    pets = []
+    for i in range(limit):
+        pets.append(generate_pet(i))
+    return pets
 
 
-app = connexion.FlaskApp(__name__, specification_dir='openapi/')
+
+def generate_pet(id: int):
+    return json.loads(f'{{"id":{id},"name":"Pet {id}","tag":"#pet{id}"}}')
+
+
+app = FlaskApp(__name__, specification_dir='openapi/')
 app.add_api('petstore.yaml', arguments={"title": "Connexion example"}, resolver=RelativeResolver("app"))
 app.run(port=8080)
